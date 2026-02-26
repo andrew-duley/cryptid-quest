85b49d99dc68e5bc81260ca98c90d5c8e41069e3
awduley <duleyalaska@gmail.com>
2026-02-26 14:20:30 -0800

    Implement session-based admin authentication with Postgres store

--- server/src/index.js
+++ server/src/index.js
@@ -149,18 +149,6 @@ app.use((err, req, res, next) => {
   res.status(status).json({ error: { message, code, status }});
 });
 
-console.log("ROUTES:");
-if (app.router && app.router.stack) {
-  app.router.stack
-    .filter(r => r.route)
-    .forEach(r => {
-      const methods = Object.keys(r.route.methods).join(",").toUpperCase();
-      console.log(`${methods} ${r.route.path}`);
-    });
-} else {
-  console.log("(router not initialized yet)");
-}
-
 app.listen(PORT, () => {
   console.log(`API running on http://localhost:${PORT}`);
 });
