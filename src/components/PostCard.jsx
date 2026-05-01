87942343adaad79309ded4799e5c53eaad8d4666
awduley <duleyalaska@gmail.com>
2026-05-01 14:07:50 -0700

    Continue refactoring styles for cards

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -18,21 +18,22 @@ export default function PostCard({ post }) {
         />
       </Link>
 
-      <section className="post-card__meta">
+      <div className="post-card__body">
+        <h3 className="post-card__title">
+          <Link to={`/the-campfire/${post.slug}`}>{post.title}</Link>
+        </h3>
+        
+        <section className="post-card__meta">
         <span className="post-card__cat pill">{post.slug}</span>
         <time dateTime={post.created_at}>
           {new Date(post.created_at).toLocaleDateString(undefined, { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' })}
         </time>
-        <span aria-hidden="true">•</span>
-        <span>{post.minutes} min read</span>
-      </section>
+        </section>
 
-      <h3 className="post-card__title">
-        <Link to={`/the-campfire/${post.slug}`}>{post.title}</Link>
-      </h3>
-
-      <p className="post-card__excerpt">{plainExcerpt}</p>
-      <Link to={`/the-campfire/${post.slug}`} className="post-card__more">Read more</Link>
+        <p className="post-card__excerpt">{plainExcerpt}</p>
+        <Link to={`/the-campfire/${post.slug}`} className="post-card__more">Read more</Link>
+      </div>
+      
     </article>
   );
 }
